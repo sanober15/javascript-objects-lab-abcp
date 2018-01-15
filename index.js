4d25b86b714672c1c40e92f320ff5ffb5ab5ab40
sanober riaz <sanober@sanober.me>
2018-01-15 02:15:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   var obj = Object.assign({}, object, {[key] : value});
   return obj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]:value});
+}
